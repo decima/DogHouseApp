caed564780abd4c64b023b43edd1b9b2fa2b8c8f
Henri Larget <henri@larget.fr>
2014-10-02 14:27:54 +0200

    indented

--- libraries.js
+++ libraries.js
@@ -5,23 +5,22 @@ var application = {};
 /**
  * @class adresse
  */
-application.Adresse = function(rue,ville,cp){
+application.Adresse = function (rue, ville, cp) {
     this.rue = rue;
     this.ville = ville;
     this.cp = cp;
 };
 application.Adresse.prototype = {
-
 };
 
 /**
  * @class Personne
  *
  */
-application.Personne = function(data){
-    this.nom = data.nom?data.nom:"";
-    this.prenom = data.prenom?data.prenom:"";
-    this.telephone = data.telephone?data.telephone:"";
+application.Personne = function (data) {
+    this.nom = data.nom ? data.nom : "";
+    this.prenom = data.prenom ? data.prenom : "";
+    this.telephone = data.telephone ? data.telephone : "";
 };
 application.Personne.prototype = {};
 
@@ -30,12 +29,12 @@ application.Personne.prototype = {};
  * @class Client
  *
  */
-application.Client = function(data){
-    this.animal = data.animal?data.animal:"";
-    this.raceAnimal = data.race?data.race:"";
-    this.nom = data.nom?data.nom:"";
-    this.prenom = data.prenom?data.prenom:"";
-    this.telephone = data.telephone?data.telephone:"";
+application.Client = function (data) {
+    this.animal = data.animal ? data.animal : "";
+    this.raceAnimal = data.race ? data.race : "";
+    this.nom = data.nom ? data.nom : "";
+    this.prenom = data.prenom ? data.prenom : "";
+    this.telephone = data.telephone ? data.telephone : "";
 };
 application.Client.prototype = new application.Personne({});
 
@@ -44,14 +43,14 @@ application.Client.prototype = new application.Personne({});
  * @class Employe
  *
  */
-application.Employe = function(data){
-    this.nom = data.nom?data.nom:"";
-    this.prenom = data.prenom?data.prenom:"";
-    this.telephone = data.telephone?data.telephone:"";
+application.Employe = function (data) {
+    this.nom = data.nom ? data.nom : "";
+    this.prenom = data.prenom ? data.prenom : "";
+    this.telephone = data.telephone ? data.telephone : "";
 };
 application.Employe.prototype = new application.Personne({});
 
 
-application.Creneau=function(){
+application.Creneau = function () {
 
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.onload = function () {
-    u1 = new application.Employe({nom:"LARGET", prenom:"Henri",telephone:"06 81 79 05 20"});
-    u2 = new application.Client({nom:"john"});
+    u1 = new application.Employe({nom: "LARGET", prenom: "Henri", telephone: "06 81 79 05 20"});
+    u2 = new application.Client({nom: "john"});
 };
